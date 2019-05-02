b5944d182ca8039acade1f8976277c37a2db00e1
willf80 <willyfkouadio@gmail.com>
2019-05-03 00:15:59 +0200

    Action bar and tab bar layout designed. In MainActivity, I set toolbar to default

--- app/src/main/java/com/appinlab/mynews/MainActivity.java
+++ app/src/main/java/com/appinlab/mynews/MainActivity.java
@@ -2,12 +2,22 @@ package com.appinlab.mynews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
     }
 }
