8178d7b25251137cc6b1e87354d43c04c296138b
willf80 <willyfkouadio@gmail.com>
2019-05-12 03:51:34 +0200

    Abstract article adapter and fragment is defined the article adapter and fragment functional logic.

---
+++ app/src/main/java/com/appinlab/mynews/adapters/AbstractArticleAdapter.java
@@ -0,0 +1,124 @@
+package com.appinlab.mynews.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.appinlab.mynews.R;
+import com.appinlab.mynews.models.Image;
+import com.appinlab.mynews.models.Media;
+import com.appinlab.mynews.models.TopStoryArticle;
+import com.appinlab.mynews.utils.DateUtils;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public abstract class AbstractArticleAdapter<T> extends RecyclerView.Adapter<AbstractArticleAdapter.ArticleItemViewHolder> {
+
+    private List<T> mArticleList;
+
+    AbstractArticleAdapter(List<T> articleList) {
+        mArticleList = articleList;
+    }
+
+    public void setArticleList(List<T> articleList) {
+        mArticleList = articleList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public AbstractArticleAdapter.ArticleItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.article_item_view,
+                viewGroup, false);
+        return new AbstractArticleAdapter.ArticleItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AbstractArticleAdapter.ArticleItemViewHolder articleItemViewHolder, int i) {
+        T article = mArticleList.get(i);
+        bind(articleItemViewHolder, article);
+    }
+
+    void showImage(Context context, Image image, ImageView intoImageView) {
+        // When image is null then return default image
+        if(image == null || image.getUrl() == null) {
+            image = new Image();
+            image.setUrl("");
+        }
+
+        String imageUrl = image.getUrl();
+        String prefix = "https://";
+        if(!imageUrl.startsWith(prefix)) {
+            imageUrl = context.getString(R.string.image_base_url) + imageUrl;
+        }
+
+        Picasso.with(context)
+                .load(imageUrl)
+                .centerCrop()
+                .resize(75, 75)
+                .placeholder(R.drawable.nytimes_placeholder)
+                .error(R.drawable.nytimes_placeholder)
+                .into(intoImageView);
+    }
+
+    // Get first image of article
+    Image getArticleImage(List<Image> imageList) {
+        Image image = null;
+
+        if(imageList != null  && imageList.size() > 0) {
+            image = imageList.get(0);
+        }
+
+        return image;
+    }
+
+    // Create breadcrumb with section and subsection
+    String doBreadCrumb(String section, String subSection) {
+        String breadcrumb = section;
+        if(subSection != null && subSection.length() > 0){
+            breadcrumb = String.format("%s > %s", section, subSection);
+        }
+
+        return  breadcrumb;
+    }
+
+    abstract void bind(AbstractArticleAdapter.ArticleItemViewHolder articleItemViewHolder, T article);
+
+    @Override
+    public int getItemCount() {
+        return mArticleList.size();
+    }
+
+    public class ArticleItemViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.imageView)
+        public ImageView mImageView;
+
+        @BindView(R.id.sectionTitleTextView)
+        public TextView mSectionTitleTextView;
+
+        @BindView(R.id.dateTextView)
+        public TextView mDateTextView;
+
+        @BindView(R.id.titleTextView)
+        public TextView mTitleTextView;
+
+        Context mContext;
+
+        ArticleItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            mContext = itemView.getContext();
+        }
+    }
+}
